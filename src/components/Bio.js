e8cc9206e8717fb6cdca11e7f7603d467fb3e565
DeepCold <deepcold@shenhans-iMac.lan>
2019-02-18 00:40:57 +0800

    add profile

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './profile.jpeg'
+import profilePic from './profile.JPG'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`DeepCold`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
