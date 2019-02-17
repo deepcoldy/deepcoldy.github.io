e359e7e330ffbc18d7300f411bd404a962f39a5f
DeepCold <deepcold@shenhans-iMac.lan>
2019-02-18 00:45:49 +0800

    add profile

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './profile.JPG'
+import profilePic from './profile.jpeg'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
