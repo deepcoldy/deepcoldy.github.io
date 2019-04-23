150da8056d95f673c5464530ed1a1bef60ddb767
80253170 <shenhan@oppo.com>
2019-04-23 17:22:47 +0800

    修改config;

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://deepcoldy.github.io/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/deepcoldy.github.io',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
